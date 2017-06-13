695f19cf71e964828abd8490c38b0383da5acbbf
lpaseen <peters-github@techwiz.ca>
2017-06-12 22:20:19 -0400

    added help, code cleanup, fixed so date show matches kennegruppe

--- GenKeyM4.sh
+++ GenKeyM4.sh
@@ -5,62 +5,66 @@
 #	Created
 #2017-01-02  Peter Sjoberg peters-src AT techwiz DOT ca
 #	Added code for 2017
+#2017-06-12  Peter Sjoberg peters-src AT techwiz DOT ca
+#	added help, code cleanup, fixed so date show matches kennegruppe
 #
-#Parameters:
-# $0 tomorrow
-#	to get key for some other day than today
-# $0 tomorrow Grp1 grp2
-#	find key using bigramtable for tomorrow
-#	BUG: date is not from kennegruppe but from "tomorrow"
-# $0 grp1 grp2
-#	find key using this months bigram table
+#
 
-#BGTABLE=${0%/*}/BigramTable.txt
 BASEPATH=$(dirname $(readlink -f $0))
 
-if [ -n "$1" ] && date -d "$1" &>/dev/null;then
+
+if [ "$1" == "-h" -o "$1" == "-?" ];then
+    echo "Usage:"
+    echo " $0 -h"
+    echo "      this help"
+    echo
+    echo " $0 tomorrow"
+    echo " $0 $(date +%F -d "2 days ago")"
+    echo "	to get key for some other day than today"
+    echo
+    echo " $0 $(date +%Y-%m -d "next month") grp1 grp2"
+    echo "	find key for a msg using bigramtable for $(date +%Y-%m -d "next month")"
+    echo
+    echo " $0 grp1 grp2"
+    echo "	find key for a msg using current months bigram table"
+    exit;
+fi
+
+P1="$1"
+echo "$P1"|grep -q "^20[0-9][0-9]-[0-1][0-9]$" && P1="$P1-01"
+echo "$P1"|grep -q "^20[0-9][0-9]-[0-9]$" && P1="$P1-01"
+
+if [ -n "$P1" ] && date -d "$P1" &>/dev/null;then
+    WHEN="$P1"
+    shift
+elif [ -n "$1" ] && date -d "$1" &>/dev/null;then
     WHEN="$1"
     shift
 else
     WHEN="now"
 fi
 
-BGTABLE=$BASEPATH/Keychart/Bigramtable_$(date -d "$WHEN" +%Y-%m)_unwrapped.txt
-BGT=$BASEPATH/Keychart/Bigramtable_$(date -d "$WHEN" +%Y-%m)
-M4TABLE=$BASEPATH/Keychart/EWCG_keysheet_M4_$(date -d "$WHEN" +%Y-%m).txt
+YEAR=$(date -d "$WHEN" +%Y)
+YM=$(date -d "$WHEN" +%Y-%m)
+DAY=$(date -d "$WHEN" +%e|tr -d ' ')
+
+BGT=$BASEPATH/Keychart/Bigramtable_${YM}
+M4TABLE=$BASEPATH/Keychart/EWCG_keysheet_M4_${YM}.txt
 
 [ ! -e "$BGT.txt" ] && echo "Bigram table $BGT.txt missing - ABORT" && exit 1
 [ ! -e "$M4TABLE" ] && echo "Keysheet $M4TABLE missing - ABORT" && exit 1
 
 if [ -n "$DEBUG" ];then
-    echo ls -l $BGTABLE
-    ls -l $BGTABLE
-    echo ls -l $M4TABLE
-    ls -l $M4TABLE
-fi
-
-if [ ! -r $BGT.txt ];then
-    echo "Can't find the bigram table at $BGT.txt"
-    exit
-fi
-
-if [ ! -r $M4TABLE ];then
-    echo "Can't find the M4 keychart table at $M4TABLE"
-    exit
-fi
-
-if [ -n "$WHEN" ];then
-    DAY=$(date -d "$WHEN" +%e|tr -d ' ')
-    if [ "$1" != "-r" -a "${#2}" -ne 4 ];then
-	KG=$(cat $M4TABLE|awk -F\| '{if ($3=='$DAY'){print $2}}'|head -1)
-	set - $KG
-    fi
+    echo "BGT=$BGT"
+    [ -e "$BGT" ] && ls -l $BGT || echo "$BGT NOT FOUND!!!"
+    echo "M4TABLE=$M4TABLE"
+    [ -e "$M4TABLE" ] && ls -l $M4TABLE || echo "$M4TABLE NOT FOUND!!!"
 fi
 
 #Check if a script friendly (unwrapped) textfile exist, if not - create it.
 BGTABLE=${BGT}_unwrapped.txt
 if [ ! -e "$BGTABLE" ];then
-    echo "Creating $BGTABLE"
+    echo "Please wait, creating $BGTABLE"
     BGLIST=""
     while read a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26;do
 	[ -z "$a26" ] && continue
@@ -77,46 +81,37 @@ $left=$right"
     echo "$BGLIST"|sort >$BGTABLE
 fi
 
-if [ -z "$1" ];then # assume we want todays key
-    DAY=$(date +%e|tr -d ' ')
-    KG=$(cat $M4TABLE|awk -F\| '{if ($3=='$DAY'){print $2}}'|head -1)
-    set - $KG
-fi
+if [ "${#2}" -eq 4 ];then
+    # We doing reverse, extract info from bigram tables
+    GR="$1$2"
 
-YEAR=$(date -d "$WHEN" +%Y)
-#Check if we doing a reverse/decrypt
-
-if [ "$1" == "-r" -o "${#2}" -eq 4 ];then
-    REV=true
-    if [ -n "$3" ];then
-	GR="$2$3"
-    else
-	GR="$1$2"
-    fi
-
-#grep -E "FU|VW|KC|BH" BigramTable.txt
     for i in 0 1 2 3;do
 	let POS=$i*2
 	BG[i]=$(grep "^${GR:$POS:2}" $BGTABLE|cut -d= -f2)
-	grep "^${GR:$POS:2}" $BGTABLE|sed 's/=/=>/'
+	echo "${GR:$POS:2}=>${BG[i]}"
     done
     echo
     KG=${BG[1]:0:1}${BG[2]:0:1}${BG[3]:0:1}
     MK=${BG[0]:1:1}${BG[1]:1:1}${BG[2]:1:1}
-    echo "$(echo ${BG[0]:0:1}|tr '[:upper:]' '[:lower:'])${BG[1]:0:1}${BG[2]:0:1}${BG[3]:0:1}"
-    echo "${BG[0]:1:1}${BG[1]:1:1}${BG[2]:1:1}$(echo ${BG[3]:1:1}|tr '[:upper:]' '[:lower:'])"
+    echo "first part : $(echo ${BG[0]:0:1}|tr '[:upper:]' '[:lower:'])${BG[1]:0:1}${BG[2]:0:1}${BG[3]:0:1}"
+    echo "second part: ${BG[0]:1:1}${BG[1]:1:1}${BG[2]:1:1}$(echo ${BG[3]:1:1}|tr '[:upper:]' '[:lower:'])"
     echo
-#    for i in 0 1 2 3;do
-#	echo ${BG[i]}
-#    done
     echo "kenngroup  = $KG"
     echo "message key= $MK"
+    #    KG=$(cat $M4TABLE|awk -F\| '{if ($3=='$DAY'){print $2}}'|head -1)
+    DAY=$(cat $M4TABLE|awk -F\| '$2 ~ /'$KG'/{print $3}'|head -1|tr -d ' ')
+    if [ -z "$DAY" ];then
+	echo "kenngroup $KG was not found in $M4TABLE - ABORT"
+	exit 3
+    fi
+    WHEN="$YM-$DAY"
+    YEAR=$(date -d "$WHEN" +%Y)
+    YM=$(date -d "$WHEN" +%Y-%m)
 else
-    KG="$(echo $1|tr '[:lower:]' '[:upper:]')"
-    MK="$(echo $2|tr '[:lower:]' '[:upper:]')"
-    [ -z "$MK" ] && MK="$(echo $(($RANDOM%26+65)) $(($RANDOM%26+65)) $(($RANDOM%26+65))|awk '{printf "%c%c%c",$1,$2,$3}')"
-    echo "kenngoup   = $KG"
-    echo "message key= $MK"
+    KG=$(cat $M4TABLE|awk -F\| '$3=='$DAY' {print $2}'|head -1|tr '[:lower:]' '[:upper:]'|tr -d ' ')
+    MK="$(echo $(($RANDOM%26+65)) $(($RANDOM%26+65)) $(($RANDOM%26+65))|awk '{printf "%c%c%c",$1,$2,$3}')"
+    echo "kenngoup   = >$KG<"
+    echo "message key= >$MK<"
     FKG=$KG
     FMK=$MK
     [ ${#FKG} -eq 3 ] && FKG="$(echo $(($RANDOM%26+97))|awk '{printf "%c",$1}')$KG"
@@ -134,10 +129,17 @@ else
 	fi
     done
     echo
-    echo ${bg[0]}${bg[1]} ${bg[2]}${bg[3]}
+    echo "First two grops are: ${bg[0]}${bg[1]} ${bg[2]}${bg[3]}"
 fi
 
-echo KG=$KG
+if [ -n "$DEBUG" ];then
+    echo KG=$KG
+    echo MK=$MK
+    echo DAY=$DAY
+    echo WHEN=$WHEN
+    echo YM=$YM
+fi
+
 if [ ${#KG} -eq 3 ];then
     INNER=$(grep -A1 $KG $M4TABLE|head -2)
     OUTER=$(grep $KG $M4TABLE|tail -1)
@@ -148,7 +150,6 @@ if [ ${#KG} -eq 3 ];then
     echo "#OUTER="$OUTER
     echo
 
-#    eval $(echo $INNER|awk '{print "KGI="$1";DAYI="$2";UKW="$3";W[0]="$4";R0="$5";W[1]="$6";R1="$7";W[2]="$8";R2="$9";W[3]="$10";R3="$11"\n"}');
     eval $(echo $INNER|tr -d \||awk '{print "KGI="$1";DAYI="$2";UKW="$3";W[0]="$4";W[1]="$5";W[2]="$6";W[3]="$7";R0="$8";R1="$9";R2="$10";R3="$11"\n"}')
 
     [ "$UKW" == "B" ] && UKW="UKWBT" || UKW="UKWCT"
@@ -173,7 +174,6 @@ if [ ${#KG} -eq 3 ];then
     RING=$R0$R1$R2$R3
 
     #XVE 10 20/15 3/5 14/7 19/12 9/4 25/26 8/2 1/16 24/21 18/23 P Z F A
-#    eval $(echo $OUTER|awk '{print "KGO="$1";DAYO="$2";PB=\""$3,$4,$5,$6,$7,$8,$9,$10,$11,$12"\";START="$13$14$15$16"\n"}')
     eval $(echo $OUTER|awk '{print "KGO="$2";DAYO="$4";PB=\""$6,$7,$8,$9,$10,$11,$12,$13,$14,$15"\";START="$17$18$19$20"\n"}')
 #    echo "#KGO=$KGO"
 #    echo "#PB=$PB"
